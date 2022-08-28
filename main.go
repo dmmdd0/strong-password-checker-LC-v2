7b1b7fc54d7b06f8b601fe7a732c53c89db5a562
dimm <witty.sock3113@fastmail.com>
2022-08-28 16:24:04 +0200

    checker OK on AAA AAAA AAAAA AAAAAA

--- main.go
+++ main.go
@@ -7,17 +7,24 @@ import (
 
 func main() {
 	var pass string
-	pass = "aaaabbbbccccddeeddeeddeedd"
 	pass = "!"
-	pass = "aaaaabbbbbbccccccddeeddeeddeedd"
+	pass = "!!!!!!!!!.........!!!!!!.!.!.!.!.!"
+	pass = "aaaaaabbbbbbccccccddeeddee" // OK
+	pass = "aaaaabbbbbcccccddeeddeedde" // OK
+	pass = "aaaabbbbccccddeeddeeddeedd" // OK
+	pass = "aaabbbcccdeeddeeddeeddeded" // OK
+
+	pass = "aaaaaabbbbbbccccccddeeddee"
 
 	fmt.Print(pass, "\n")
 	//fmt.Println(repDelTest(pass))
 	fmt.Println(clusterAnalyzer(pass))
+	fmt.Println(strongPasswordChecker(pass))
+
 }
 
 type repDel struct {
-	replaseMust int
+	replaceMust int
 	deleteMust  int
 }
 
@@ -36,17 +43,110 @@ const (
 //func repDelTest(p string) repDel {
 //
 //	a := repDel{
-//		replaseMust: len(p),
+//		replaceMust: len(p),
 //		deleteMust:  len(p) / 3,
 //	}
 //	return a
 //}
 
+func strongPasswordChecker(password string) int {
+	var sheat int
+	//
+	c := clusterAnalyzer(password)
+	if c.repit {
+		for i, _ := range c.cluster {
+
+			// replace except last one in cluster
+			if c.upLowDig > 0 && (c.cluster[i].deleteMust+2)%3 != 0 {
+				r := c.cluster[i].replaceMust
+				c.cluster[i].deleteMust = (c.cluster[i].deleteMust+2)/3 - r
+				c.cluster[i].replaceMust -= r
+				c.upLowDig -= r
+				sheat += r
+			}
+
+			// replace last for AAA, AAAAAA,
+			if c.upLowDig > 0 && c.cluster[i].replaceMust > 1 && (c.cluster[i].deleteMust+2)%3 == 0 {
+				var r int
+				if c.upLowDig >= c.cluster[i].replaceMust {
+					r = c.cluster[i].replaceMust - 1
+				} else {
+					r = c.upLowDig
+				}
+
+				c.cluster[i].deleteMust = (c.cluster[i].deleteMust+2)/3 - r
+				c.cluster[i].replaceMust -= r
+				c.upLowDig -= r
+				sheat += r
+			}
+
+			//if c.upLowDig > 0 && c.cluster[i].replaceMust >= 2 {
+			//	r := c.cluster[i].replaceMust - 1
+			//	c.cluster[i].deleteMust = (c.cluster[i].deleteMust+2)/3 - r
+			//	c.cluster[i].replaceMust -= r
+			//	c.upLowDig -= r
+			//	sheat += r
+			//}
+
+			// delete last one
+			if c.upLowDig > 0 && c.cluster[i].deleteMust == 1 {
+				sheat++
+				c.cluster[i].replaceMust = 0
+				c.cluster[i].deleteMust = 0
+				//c.upLowDig--
+				c.tail--
+			}
+
+			// delete last two for aaaa [{1 2}]
+			if c.upLowDig > 0 && c.cluster[i].deleteMust == 2 {
+				c.cluster[i].replaceMust = 0
+				c.cluster[i].deleteMust = 0
+				sheat += 2
+				c.tail -= 2
+			}
+
+			// replace all except last one
+			// todo: not working for AAA, AAAAAA,...
+			//	don't work at all
+			//if c.upLowDig == 0 && c.cluster[i].replaceMust >= 2 {
+			//	r := c.cluster[i].replaceMust - 1
+			//	c.cluster[i].deleteMust = (c.cluster[i].deleteMust+2)/3 - r
+			//	c.cluster[i].replaceMust -= r
+			//	sheat += r
+			//}
+
+			// delete last
+			//if c.upLowDig == 0 && c.cluster[i].replaceMust == 1 && c.cluster[i].deleteMust <= c.tail {
+			if c.upLowDig == 0 && c.cluster[i].deleteMust != 0 && c.cluster[i].deleteMust <= c.tail {
+				sheat += c.cluster[i].deleteMust
+				c.tail -= c.cluster[i].deleteMust
+				c.cluster[i].deleteMust = 0
+				c.cluster[i].replaceMust = 0
+			}
+
+		}
+
+		// if not replaced during clusters iteration
+		if c.upLowDig != 0 {
+			sheat += c.upLowDig
+			c.upLowDig = 0
+		}
+
+		//	delete tail
+		if c.tail > 0 {
+			sheat += c.tail
+			c.tail = 0
+		}
+	}
+	fmt.Println(c)
+	return sheat
+}
+
 func clusterAnalyzer(p string) clusterStatus {
 	//var a []repDel
 	//
 	//a = append(a, repDel{
-	//	replaseMust: len(p),
+	//	replaceMust: len(p),
 	//	deleteMust:  len(p) / 3,
 	//})
 	//
@@ -75,7 +175,7 @@ func clusterAnalyzer(p string) clusterStatus {
 		if clusterCounter >= 3 && (char != v || i+1 == l) {
 
 			cluster = append(cluster, repDel{
-				replaseMust: clusterCounter / 3,
+				replaceMust: clusterCounter / 3,
 				deleteMust:  clusterCounter - 2,
 			})
 
@@ -113,6 +213,8 @@ func clusterAnalyzer(p string) clusterStatus {
 
 	}
 	clusterito.upLowDig = up + low + dig
+
+	clusterito.tail = l - max
 
 	return clusterito
 }
